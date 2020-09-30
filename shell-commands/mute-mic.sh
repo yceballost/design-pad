c1a1f8a607ea5f6637ca82559505b18fb9afd91c
gh-actions <actions@gihub.com>
2020-09-30 17:41:08 +0200

    upload

--- shell-commands/mute-mic.sh
+++ shell-commands/mute-mic.sh
@@ -1,9 +1,15 @@
-set inputVolume to input volume of (get volume settings)
-if inputVolume = 0 then
-    tell application "System Events" to set volume input volume 0
-    osascript -e 'display notification "Mic ON"'
+on getMicrophoneVolume()
+	input volume of (get volume settings)
+end getMicrophoneVolume
+on disableMicrophone()
+	set volume input volume 0
+end disableMicrophone
+on enableMicrophone()
+	set volume input volume 100
+end enableMicrophone
+
+if getMicrophoneVolume() is greater than 0 then
+	disableMicrophone()
 else
-    tell application "System Events" to set volume input volume 100
-    osascript -e 'display notification "Mic OFF"'
+	enableMicrophone()
 end if
-set volume input volume inputVolume
